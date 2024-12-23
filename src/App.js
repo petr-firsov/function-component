9fde84d33921e400eb6e98ff36ad6477ba6fc010
petr-firsov <125796227+petr-firsov@users.noreply.github.com>
2024-12-23 23:15:47 +0300

    Первый вариант

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { ShopItemFunc } from './ShopItemFunc';
+
+
+function App() {
+  return (
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ShopItemFunc />
+      </div>
+    </div>
+  );
+}
+
+export default App;
